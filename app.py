e596421e1dd3ee5fc1d308cf2e22349c9e9666e1
Vedant Mahalle <vedantmahalle39@gmail.com>
2025-07-02 02:24:30 +0530

    UI changes

--- app.py
+++ app.py
@@ -104,4 +104,6 @@ def recommend():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # use Render's provided port
+    app.run(host='0.0.0.0', port=port, debug=True)
